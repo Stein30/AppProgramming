c1b9caf453a13a5bb8ce5b94b21873237fc848f6
Mio Shimada <38175854+Stein30@users.noreply.github.com>
2019-11-25 16:02:27 +0900

    Add files via upload

---
+++ BarabaraGame-master/BarabaraGame/ResultViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ResultViewController.swift
+//  BarabaraGame
+//
+//  Created by Mio on 2018/04/14.
+//  Copyright © 2018年 Mio. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet var rankingLabel1: UILabel!
+    @IBOutlet var rankingLabel2: UILabel!
+    @IBOutlet var rankingLabel3: UILabel!
+    
+    let defaults: UserDefaults = UserDefaults.standard
+    
+    @IBAction func toTop(){
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rankingLabel1.text = String(defaults.integer(forKey: "score1"))
+        rankingLabel2.text = String(defaults.integer(forKey: "score2"))
+        rankingLabel3.text = String(defaults.integer(forKey: "score3"))
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BarabaraGame-master/BarabaraGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarabaraGame
+//
+//  Created by Mio on 2018/04/14.
+//  Copyright © 2018年 Mio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
